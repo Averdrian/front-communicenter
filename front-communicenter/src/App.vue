dc19a6cf12b15ab86e6998a447e1c243e2190afd
Adrian <adrianestevezg@gmail.com>
2023-09-25 14:13:17 +0200

    setup vue-cli

---
+++ front-communicenter/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #ffffff;
+  background-color: #323232;
+}
+
+body {
+  margin: 0px;
+}
+
+#app {
+  height: 100vh;
+}
+</style>
