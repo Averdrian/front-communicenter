dc367e40978f2b2671945a0430e380f8e3c20fe2
Adrian <adrianestevezg@gmail.com>
2023-10-05 14:52:43 +0200

    simple api call

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,10 @@ export default {
   name: 'App',
   components: {
     // routerView
-  }
+  },
+
+
+
 }
 </script>
 
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -8,7 +8,25 @@
 <script>
 
     export default {
-        name:"Login"
+        name:"Login",
+
+        mounted() {
+            console.log("BBASDA");
+        },
+
+        async created() {
+            await this.access();
+        },
+
+        methods : {
+
+            async access() {
+                let data;
+                data = (await fetch('http://localhost:8000/api/data'));
+                console.log(data);
+            }
+
+        }
     };
 
 </script>
